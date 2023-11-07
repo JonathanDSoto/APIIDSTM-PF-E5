27bb2208f78c872510e1f0f510cc94fce483a00e
alexander <hsa.lapaz@gmail.com>
2023-11-07 16:55:55 -0700

    BACKEND - Se empezo a crear las columnas y relaciones de la bdd

--- database/migrations/2023_11_07_232411_create_products_table.php
+++ database/migrations/2023_11_07_232411_create_products_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->longText('description')->nullable();
+            $table->string('model')->nullable();
+            $table->string('brand')->nullable();
+            $table->string('release_date', 4)->nullable();
+            $table->string('color');
+            $table->string('price')->nullable();
             $table->timestamps();
         });
     }
