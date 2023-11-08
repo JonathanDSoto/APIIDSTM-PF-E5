e06671d86ee3ff5c8d2f837ee93c933f9734586d
¨retbleed¨ <hsa.lapaz@gmail.com>
2023-11-08 11:50:28 -0700

    BACKEND - Se agregaron ciertas rutas para el maquetador

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function(){
+    return view();
+});
+
+Route::get('/login', function(){
+    return view();
+});
+
+Route::get('/dashboard', function() {
+    return view();
 });
