f7b39034af562b05ecf262ab14edf9419284c614
alexander <hsa.lapaz@gmail.com>
2023-11-29 01:33:15 -0700

    BACKEND - Se agrego placeholder a las imagenes

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,16 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Storage;
 
 class CategoryController extends Controller
 {
+    // ...
     /**
      * Display a listing of the resource.
      */
-    public function index()
-    {
+    public function index(){
         //
+        return view('category.index', [
+            'categories' => Category::all()
+        ]);
+    }
 
+    public function home(){
+        //
         return view('home', [
             'categories' => Category::all()
         ]);
@@ -22,8 +30,7 @@ class CategoryController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
-    {
+    public function create(){
         //
         return view('category.create');
     }
@@ -33,20 +40,29 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-        $imagen = $request->file('image_url')->store('public/images');
-        $url = asset($imagen);
-        $request->merge(['image_url' => $url]);
-        error_log($url);
-        //
+        $response = Http::get('https://placehold.co/600x400/000000/FFFFFF/png');
+        if ($response->ok()) {
+            $url = $response->url();
+            $request->merge(['image_url' => $url]);
+        }
+
         $validatedData = $request->validate([
             'name' => 'required|max:255',
-            'image_url' => 'required|image|max:2048'
+            'image_url' => 'image|max:2048'
         ]);
+
+        $imagePath = $request->file('image_url')->store('public/images');
+        $imageUrl = null;
+
+        if (!Storage::exists($imagePath)) {
+            $imageUrl = asset(Storage::url($imagePath));
+        }
+
         Category::create([
             'name' => $validatedData['name'],
-            'image_url' => $validatedData['image_url']
+            'image_url' => $imageUrl
         ]);
-        return redirect(route('category.index'));
+        return redirect(route('home'));
     }
 
     /**
@@ -80,11 +96,11 @@ class CategoryController extends Controller
         //
         $validatedData = $request->validate([
             'name' => 'required|max:255',
-            'image_url' => 'required|max:255'
+            /* 'image_url' => 'max:255' */
         ]);
         $category->update([
             'name' => $validatedData['name'],
-            'image_url' => $validatedData['image_url']
+            /* 'image_url' => $validatedData['image_url'] */
         ]);
         return redirect(route('category.index'))->with('message', 'Categoria actualizado correctamente');
     }
