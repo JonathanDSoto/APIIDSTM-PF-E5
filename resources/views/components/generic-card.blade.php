78385a8c856db30d68f0dbdc90c37d18b17a988d
alexander <hsa.lapaz@gmail.com>
2023-12-01 15:31:27 -0700

    BACKEND - Se empezo a trabajar con las reservaciones, hotfix de servicios

--- resources/views/components/generic-card.blade.php
+++ resources/views/components/generic-card.blade.php
@@ -1,22 +1,12 @@
 <div class="card col p-0 shadow position-relative" style="width: 18rem">
     <div class="dropdown position-absolute end-0">
-        <button
-            class="btn btn-primary p-1"
-            id="actions"
-            data-bs-toggle="dropdown"
-            type="button"
-            data-bs-display="static"
-        >
+        <button class="btn btn-primary p-1" id="actions" data-bs-toggle="dropdown" type="button" data-bs-display="static">
             <i class="tf-icons ti ti-menu"></i>
         </button>
         <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="actions">
             <li>
                 <div class="dropdown-item p-0">
-                    <button
-                        class="btn btn-primary"
-                        style="width: 100%"
-                        type="button"
-                    >
+                    <button class="btn btn-primary" style="width: 100%" type="button">
                         <i class="menu-icon tf-icons ti ti-pencil"></i>
                         <span>Edit</span>
                     </button>
@@ -24,11 +14,7 @@
             </li>
             <li>
                 <div class="dropdown-item p-0">
-                    <button
-                        class="btn btn-danger"
-                        style="width: 100%"
-                        type="button"
-                    >
+                    <button class="btn btn-danger" style="width: 100%" type="button">
                         <i class="menu-icon tf-icons ti ti-trash"></i>
                         <span>Delete</span>
                     </button>
@@ -36,16 +22,12 @@
             </li>
         </ul>
     </div>
-    <img
-        src="{{ asset('assets/img/machape/fail.png') }}"
-        class="card-img-top"
-        alt="Image of {{ $title }}"
-    />
+    <img src="{{ asset('assets/img/machape/fail.png') }}" class="card-img-top" alt="Image of {{ $title }}" />
     <div class="card-body">
         <h5 class="card-title">{{ $title }}</h5>
         <p class="card-text">{{ $description }}</p>
-        @if($price !== '')
-        <p class="card-text">${{ $price }}</p>
+        @if ($price !== '')
+            <p class="card-text">${{ $price }}</p>
         @endif
         <a href="{{ $route }}" class="btn btn-primary"> Detalles </a>
     </div>
