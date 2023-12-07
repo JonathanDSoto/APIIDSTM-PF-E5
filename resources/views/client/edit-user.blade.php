270df82088fa5277569cf1d4c61276fadca552a9
MytsuLV <alixus15@gmail.com>
2023-12-07 13:33:25 -0700

    Front-End: Se valido los imputs con datos necesarios y mostrando mensajes de error.

--- resources/views/client/edit-user.blade.php
+++ resources/views/client/edit-user.blade.php
@@ -1,4 +1,4 @@
-<form action="" method="POST" class="modal fade" id="modalEdit" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<form action="{{ route('client.update', ['client' => $client->id]) }}" method="POST" class="modal fade" id="modalEdit" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" onsubmit="return validateEditForm()">
     @csrf
     @method('PUT')
     <div class="modal-dialog modal-dialog-centered">
@@ -10,6 +10,9 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
+                <div id="editValidationErrors" class="alert alert-danger" style="display: none;"></div>
+                <div id="editSuccessMessage" class="alert alert-success" style="display: none;">Client successfully updated!</div>
+
                 <div>
                     <div class="mb-3">
                         <label for="name-edit" class="col-form-label">Name:</label>
@@ -39,4 +42,75 @@
             </div>
         </div>
     </div>
+
+    <script>
+        function validateEditForm() {
+            document.getElementById('editValidationErrors').style.display = 'none';
+            document.getElementById('editValidationErrors').innerHTML = '';
+            document.getElementById('editSuccessMessage').style.display = 'none';
+
+            var name = document.getElementById('name-edit').value.trim();
+            var lastname = document.getElementById('last-name-edit').value.trim();
+            var email = document.getElementById('email-edit').value.trim();
+            var phone = document.getElementById('phone-number-edit').value.trim();
+
+            var errors = [];
+
+            if (name === '') {
+                errors.push('Name is required.');
+            }
+
+            if (lastname === '') {
+                errors.push('Last name is required.');
+            }
+
+            if (email === '') {
+                errors.push('Email is required.');
+            } else if (!isValidEmail(email)) {
+                errors.push('Invalid email address.');
+            }
+
+            if (phone === '') {
+                errors.push('Phone number is required.');
+            } else if (!isValidPhone(phone)) {
+                errors.push('Phone number must contain only digits.');
+            } else if (phone.length !== 10) {
+                errors.push('Phone number must be 10 digits.');
+            }
+
+            if (errors.length > 0) {
+                displayEditValidationErrors(errors);
+                return false;
+            } else {
+                displayEditSuccessMessage();
+                return true;
+            }
+        }
+
+        function displayEditValidationErrors(errors) {
+            var editValidationErrorsDiv = document.getElementById('editValidationErrors');
+            var errorsHtml = '';
+            errors.forEach(function (error) {
+                errorsHtml += '<li>' + error + '</li>';
+            });
+            errorsHtml += '</ul>';
+            editValidationErrorsDiv.innerHTML = errorsHtml;
+            editValidationErrorsDiv.style.display = 'block';
+        }
+
+        function displayEditSuccessMessage() {
+            var editSuccessMessageDiv = document.getElementById('editSuccessMessage');
+            editSuccessMessageDiv.style.display = 'block';
+        }
+
+        function isValidEmail(email) {
+            var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            return emailRegex.test(email);
+        }
+
+        function isValidPhone(phone) {
+            var phoneRegex = /^\d+$/;
+            return phoneRegex.test(phone);
+        }
+    </script>
 </form>
