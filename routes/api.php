121a6785f9bcb0050561ee2536eb218f8d539222
alexander <hsa.lapaz@gmail.com>
2023-12-08 13:53:00 -0700

    BACKEND - Fix api

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ use App\Http\Controllers\API\UserController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/* Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
@@ -41,47 +41,50 @@ Route::post('/login', function (Request $request) {
 Route::group(['middleware' => ['auth:sanctum']], function () {
 
     // Crud de los clientes
-    Route::group(['prefix' => 'clients'], function () {
-        Route::get('/', [ClientController::class, 'index'])->name('client.index');
-        Route::get('/create', [ClientController::class, 'create'])->name('client.create');
-        Route::post('/store', [ClientController::class, 'store'])->name('client.store');
-        Route::get('/{client}', [ClientController::class, 'show'])->name('client.show');
-        Route::get('/{client}/edit', [ClientController::class, 'edit'])->name('client.edit');
-        Route::put('/{client}/update', [ClientController::class, 'update'])->name('client.update');
-        Route::delete('/{client}/destroy', [ClientController::class, 'destroy'])->name('client.destroy');
-    });
 
-    // Crud de las categorias
-    Route::group(['prefix' => 'categories'], function () {
-        Route::get('/', [CategoryController::class, 'index'])->name('category.index');
-        Route::get('/create', [CategoryController::class, 'create'])->name('category.create');
-        Route::post('/store', [CategoryController::class, 'store'])->name('category.store');
-        Route::get('/{category}', [CategoryController::class, 'show'])->name('category.show');
-        Route::get('/{category}/edit', [CategoryController::class, 'edit'])->name('category.edit');
-        Route::put('/{category}/update', [CategoryController::class, 'update'])->name('category.update');
-        Route::delete('/{category}/destroy', [CategoryController::class, 'destroy'])->name('category.destroy');
-    });
+}); */
 
-    // Crud de los servicios
-    Route::group(['prefix' => 'services'], function () {
-        Route::get('/', [ServicesController::class, 'index'])->name('service.index');
-        Route::get('/create', [ServicesController::class, 'create'])->name('service.create');
-        Route::post('/store', [ServicesController::class, 'store'])->name('service.store');
-        Route::get('/{service}', [ServicesController::class, 'show'])->name('service.show');
-        Route::get('/{service}/edit', [ServicesController::class, 'edit'])->name('service.edit');
-        Route::put('/{service}/update', [ServicesController::class, 'update'])->name('service.update');
-        Route::delete('/{service}/destroy', [ServicesController::class, 'destroy'])->name('service.destroy');
-    });
 
-    // Crud de las reservaciones
-    Route::group(['prefix' => 'reservations'], function () {
-        Route::get('/', [ReservationController::class, 'index'])->name('reservation.index');
-        Route::get('/create', [ReservationController::class, 'create'])->name('reservation.create');
-        Route::post('/store', [ReservationController::class, 'store'])->name('reservation.store');
-        Route::get('/{reservation}', [ReservationController::class, 'show'])->name('reservation.show');
-        Route::get('/{reservation}/edit', [ReservationController::class, 'edit'])->name('reservation.edit');
-        Route::put('/{reservation}/update', [ReservationController::class, 'update'])->name('reservation.update');
-        Route::delete('/{reservation}/destroy', [ReservationController::class, 'destroy'])->name('reservation.destroy');
-        Route::get('/download/{reservation}', [ReservationController::class, 'download'])->name('reservation.pdf');
-    });
+Route::group(['prefix' => 'clients'], function () {
+    Route::get('/', [ClientController::class, 'index'])->name('client.index');
+    Route::get('/create', [ClientController::class, 'create'])->name('client.create');
+    Route::post('/store', [ClientController::class, 'store'])->name('client.store');
+    Route::get('/{client}', [ClientController::class, 'show'])->name('client.show');
+    Route::get('/{client}/edit', [ClientController::class, 'edit'])->name('client.edit');
+    Route::put('/{client}/update', [ClientController::class, 'update'])->name('client.update');
+    Route::delete('/{client}/destroy', [ClientController::class, 'destroy'])->name('client.destroy');
+});
+
+// Crud de las categorias
+Route::group(['prefix' => 'categories'], function () {
+    Route::get('/', [CategoryController::class, 'index'])->name('category.index');
+    Route::get('/create', [CategoryController::class, 'create'])->name('category.create');
+    Route::post('/store', [CategoryController::class, 'store'])->name('category.store');
+    Route::get('/{category}', [CategoryController::class, 'show'])->name('category.show');
+    Route::get('/{category}/edit', [CategoryController::class, 'edit'])->name('category.edit');
+    Route::put('/{category}/update', [CategoryController::class, 'update'])->name('category.update');
+    Route::delete('/{category}/destroy', [CategoryController::class, 'destroy'])->name('category.destroy');
+});
+
+// Crud de los servicios
+Route::group(['prefix' => 'services'], function () {
+    Route::get('/', [ServicesController::class, 'index'])->name('service.index');
+    Route::get('/create', [ServicesController::class, 'create'])->name('service.create');
+    Route::post('/store', [ServicesController::class, 'store'])->name('service.store');
+    Route::get('/{service}', [ServicesController::class, 'show'])->name('service.show');
+    Route::get('/{service}/edit', [ServicesController::class, 'edit'])->name('service.edit');
+    Route::put('/{service}/update', [ServicesController::class, 'update'])->name('service.update');
+    Route::delete('/{service}/destroy', [ServicesController::class, 'destroy'])->name('service.destroy');
+});
+
+// Crud de las reservaciones
+Route::group(['prefix' => 'reservations'], function () {
+    Route::get('/', [ReservationController::class, 'index'])->name('reservation.index');
+    Route::get('/create', [ReservationController::class, 'create'])->name('reservation.create');
+    Route::post('/store', [ReservationController::class, 'store'])->name('reservation.store');
+    Route::get('/{reservation}', [ReservationController::class, 'show'])->name('reservation.show');
+    Route::get('/{reservation}/edit', [ReservationController::class, 'edit'])->name('reservation.edit');
+    Route::put('/{reservation}/update', [ReservationController::class, 'update'])->name('reservation.update');
+    Route::delete('/{reservation}/destroy', [ReservationController::class, 'destroy'])->name('reservation.destroy');
+    Route::get('/download/{reservation}', [ReservationController::class, 'download'])->name('reservation.pdf');
 });
